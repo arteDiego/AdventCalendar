9297fe8a86b24ddb013f98e05c766ccbb142cb4e
arteDiego <artediego2001@gmail.com>
2024-12-10 17:03:35 +0100

    09 part 2

--- 09/09.py
+++ 09/09.py
@@ -1,4 +1,6 @@
-with open('input_09.txt', 'r') as archivo:
+import numpy as np
+
+with open('input_09_test.txt', 'r') as archivo:
     num_str = archivo.read().strip()
     num = [int(digit) for digit in num_str]
 
@@ -47,10 +49,84 @@ def checksum(base):
         i += 1
     return sum
 
+def createBaseAndSet(num):
+    ID = 0;
+    base = []
+    set = {}
+    for i in range(len(num)):
+        if(i % 2 == 0):
+            base.append([ID] * num[i])
+            set[ID] = num[i]
+            ID += 1;
+        else:
+            base.append(['.'] * num[i])
+    
+    base = [elem for row in base for elem in row]
+    return base, set
+
+def avanzarIndice(base, index):
+    while(index < len(base) and str(base[index]) != '.'):
+        index += 1;
+    indexGap = index
+    numGaps = 0;
+
+    while(index < len(base) and str(base[index]) == '.'):
+        numGaps += 1
+        index += 1
+    finish = index >= len(base)
+
+
+    return indexGap, numGaps, index, finish
+
+def fill(base, indexGap, numGaps, set):
+    mod = False
+    selectedNum = None
+    for num, size in sorted(set.items(), key=lambda x: x[0], reverse=True):
+        if size <= numGaps:
+            selectedNum = num
+            break
+   
+    if selectedNum is None:
+        return base, False
+
+    indexNum = base.index(selectedNum)
+
+    while(indexNum < len(base) and base[indexNum] == selectedNum and indexNum > indexGap):
+        mod = True
+        base = swap(base, indexGap, indexNum)
+        indexGap += 1
+        indexNum += 1
+    
+    del set[selectedNum]
+
+    return base, mod
+
+
+def fillGaps(base, set):
+    index = 0;
+
+    while True:
+        nextloop = False
+        indexGap, numGaps, index, finish = avanzarIndice(base, index)
+        while not finish:
+            base, mod = fill(base, indexGap, numGaps, set)
+            if(mod):
+                nextloop = True
+            indexGap, numGaps, index, finish = avanzarIndice(base, index)
+
+        if not nextloop:
+            break
+    return base
+
 
             
 
 base = createBase(num)
 base = exchange(base)
+sum = checksum(base)
+print('El resultado de la suma es: ', sum)
+
+base, set = createBaseAndSet(num)
+base = fillGaps(base, set)
 sum = checksum(base)
 print('El resultado de la suma es: ', sum)
